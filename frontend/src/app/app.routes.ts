3a0a33a3a78fe2bb6f481e164d1f8935606c6b98
pavelda2 <32357716+pavelda2@users.noreply.github.com>
2026-02-20 18:33:55 +0100

    Prevent showing login for authenticated users

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,41 @@
-import { CanActivateFn, Router, Routes } from '@angular/router';
-import { inject } from '@angular/core';
-import { RecipeApiService } from './recipe-api.service';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RecipeListComponent } from './pages/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component';
-import { RecipeEditorComponent } from './pages/recipe-editor/recipe-editor.component';
-import { CookingModeComponent } from './pages/cooking-mode/cooking-mode.component';
+import { inject } from '@angular/core'
+import { CanActivateFn, Router, Routes } from '@angular/router'
+import { CookingModeComponent } from './pages/cooking-mode/cooking-mode.component'
+import { HomeComponent } from './pages/home/home.component'
+import { LoginComponent } from './pages/login/login.component'
+import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component'
+import { RecipeEditorComponent } from './pages/recipe-editor/recipe-editor.component'
+import { RecipeListComponent } from './pages/recipe-list/recipe-list.component'
+import { RecipeApiService } from './recipe-api.service'
 
 const authGuard: CanActivateFn = () => {
-  const api = inject(RecipeApiService);
-  const router = inject(Router);
+  const api = inject(RecipeApiService)
+  const router = inject(Router)
 
-  if (api.user()) {
-    return true;
+  if (api.authLoading()) {
+    return true
   }
 
-  return router.parseUrl('/login');
-};
+  if (api.user()) {
+    return true
+  }
+
+  return router.parseUrl('/login')
+}
+
+const loginGuard: CanActivateFn = () => {
+  const api = inject(RecipeApiService)
+  const router = inject(Router)
+
+  if (api.user()) {
+    return router.parseUrl('/')
+  }
+
+  return true
+}
 
 export const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', canActivate: [loginGuard], component: LoginComponent },
   { path: '', canActivate: [authGuard], component: HomeComponent },
   { path: 'recipes', canActivate: [authGuard], component: RecipeListComponent },
   { path: 'recipes/new', canActivate: [authGuard], component: RecipeEditorComponent },
@@ -28,4 +43,4 @@ export const appRoutes: Routes = [
   { path: 'recipes/:id/edit', canActivate: [authGuard], component: RecipeEditorComponent },
   { path: 'recipes/:id/cook', canActivate: [authGuard], component: CookingModeComponent },
   { path: '**', redirectTo: '' },
-];
+]
